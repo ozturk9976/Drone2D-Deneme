8418a4fcfc2a3f1ac5caf2b2c4b78f97dc703af8
ozturk9976 <74475115+ozturk9976@users.noreply.github.com>
2022-05-05 02:43:58 +0300

    update

--- Assets/Scripts/PlaneMovement.cs
+++ Assets/Scripts/PlaneMovement.cs
@@ -7,7 +7,7 @@ public class PlaneMovement : MonoBehaviour
 {
    
     private Rigidbody2D rb;
-    const float hareketGucu = 25;
+    const float hareketGucu = 45;
     
     
     void Start()
@@ -17,24 +17,8 @@ public class PlaneMovement : MonoBehaviour
     }
     void Update()
     {
-    Vector3 position = transform.position; //şu anki konum
+    
 
-    float yatayInput = Input.GetAxis("Horizontal");
-    float dikeyInput = Input.GetAxis("Vertical");
-
-
-    if (yatayInput !=0)
-    {
-        position.x += yatayInput * hareketGucu * Time.deltaTime;
-    }
-    if (dikeyInput !=0)
-    {
-        position.y += dikeyInput * hareketGucu * Time.deltaTime;
-    }
-    transform.position = position;
-
-        
-      
 
     }
     void FixedUpdate()
@@ -58,6 +42,21 @@ public class PlaneMovement : MonoBehaviour
         //         Vector2 position = this.transform.position;
         //         position.y--;
         //         this.transform.position = position;
+        Vector3 position = transform.position; //şu anki konum
+
+    float yatayInput = Input.GetAxis("Horizontal");
+    float dikeyInput = Input.GetAxis("Vertical");
+
+
+    if (yatayInput !=0)
+    {
+        position.x += yatayInput * hareketGucu * Time.deltaTime;
+    }
+    if (dikeyInput !=0)
+    {
+        position.y += dikeyInput * hareketGucu * Time.deltaTime;
+    }
+    transform.position = position;
                             
                             
         }
